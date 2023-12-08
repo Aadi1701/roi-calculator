9c8d0f2b9224108ec8c17c99364721f9c7ac6638
Aadi1701 <adityacse17@gmail.com>
2023-12-08 11:14:20 +0530

    created roi calculator

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import { Homepage } from './Container/Homepage/homepage';
+
+function App() {
+  return (
+    <Homepage/>
+  );
+}
+
+export default App;
